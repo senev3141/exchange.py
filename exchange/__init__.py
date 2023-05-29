36c04e73d95cc8b500a32af0613c376fd65850c9
senev314 <91662824+senev3141@users.noreply.github.com>
2023-05-30 00:14:36 +0200

    removed serial and cycle stage versioning

--- exchange/__init__.py
+++ exchange/__init__.py
@@ -12,7 +12,7 @@ __title__ = "exchange"
 __author__ = "Senev"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2023-present Senev"
-__version__ = "0.1.0a"
+__version__ = "0.1.0"
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 import logging
@@ -23,11 +23,9 @@ class VersionInfo(NamedTuple):
     major: int
     minor: int
     micro: int
-    releaselevel: Literal["alpha", "beta", "candidate", "final"]
-    serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
+version_info: VersionInfo = VersionInfo(major=0, minor=1, micro=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
